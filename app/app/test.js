2aa822d07f66e36c4000a84b0d9361d1f13f23ce
Jared Jordan <Jared Jordan>
2017-04-01 06:39:48 +0200

    add irc token environment variable support on testing

--- app/app/test.js
+++ app/app/test.js
@@ -4,6 +4,8 @@ function Test(Settings) {
     Settings.irc.options.port = 6667;
     Settings.irc.options.secure = false;
     Settings.irc.options.certExpired = true;
+    Settings.irc.auth.secret = process.env.IRC_TOKEN;
+    Settings.irc.auth.commandFormat = '%c %u %s';
 }
 
 module.exports = Test;
