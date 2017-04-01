9f100e1914068ec46fd5730c03060c9dd9bb33a2
Jay Bricksoft <jay-bricksoft@users.noreply.github.com>
2017-04-01 02:53:39 +0200

    Update appveyor_mongodb.ps1

--- appveyor_mongodb.ps1
+++ appveyor_mongodb.ps1
@@ -29,7 +29,7 @@ Clear-Host
 $AddedLocation ="$env:temp\mongo\"
 $Reg = "Registry::HKLM\System\CurrentControlSet\Control\Session Manager\Environment"
 $OldPath = (Get-ItemProperty -Path "$Reg" -Name PATH).Path
-$NewPath= $OldPath + ’;’ + $AddedLocation
+$NewPath= $OldPath + ';' + $AddedLocation
 Set-ItemProperty -Path "$Reg" -Name PATH –Value $NewPath
 # Return to last location, to run the build
 Pop-Location
