6c8aaa002f57341a224239b2720f3dc118505563
Jared Jordan <Jared Jordan>
2017-04-01 07:55:22 +0200

    finished heroku integration

--- app/app/test.js
+++ app/app/test.js
@@ -6,8 +6,6 @@ function Test(Settings) {
     Settings.irc.options.certExpired = true
     Settings.irc.auth.secret = process.env.IRC_TOKEN
     Settings.irc.auth.commandFormat = '%c %u %s'
-        // if on heroku (testcase) we need to set web_port using value of $PORT
-    console.log('heroku web port: ', process.env.PORT)
     Settings.web.port = process.env.PORT || Settings.web.port
 }
 
