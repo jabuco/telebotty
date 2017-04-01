9ee4c63b64a68d6b91fb5e71d0c5fc29a636b484
Jared Jordan <Jared Jordan>
2017-04-01 07:05:09 +0200

    heroku...

--- app/app/test.js
+++ app/app/test.js
@@ -7,7 +7,7 @@ function Test(Settings) {
     Settings.irc.auth.secret = process.env.IRC_TOKEN
     Settings.irc.auth.commandFormat = '%c %u %s'
         // if on heroku (testcase) we need to set web_port using value of $PORT
-    console.log('heroku web port: ' + process.env.PORT)
+    console.log('heroku web port: ', process.env.PORT)
     Settings.web.port = process.env.PORT || Settings.web.port
 }
 
