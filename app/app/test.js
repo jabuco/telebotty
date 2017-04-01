354da171e09a31c145871776f61e5e5625c228d4
Jared Jordan <Jared Jordan>
2017-04-01 06:50:35 +0200

    fix heroku

--- app/app/test.js
+++ app/app/test.js
@@ -1,11 +1,13 @@
 function Test(Settings) {
-    Settings.irc.server = 'irc.freenode.net';
-    Settings.irc.options.channels = ['#telebotty'];
-    Settings.irc.options.port = 6667;
-    Settings.irc.options.secure = false;
-    Settings.irc.options.certExpired = true;
-    Settings.irc.auth.secret = process.env.IRC_TOKEN;
-    Settings.irc.auth.commandFormat = '%c %u %s';
+    Settings.irc.server = 'irc.freenode.net'
+    Settings.irc.options.channels = ['#telebotty']
+    Settings.irc.options.port = 6667
+    Settings.irc.options.secure = false
+    Settings.irc.options.certExpired = true
+    Settings.irc.auth.secret = process.env.IRC_TOKEN
+    Settings.irc.auth.commandFormat = '%c %u %s'
+        // if on heroku (testcase) we need to set web_port using value of $PORT
+    Settings.web.port = process.env.PORT || Settings.web.port
 }
 
-module.exports = Test;
+module.exports = Test
